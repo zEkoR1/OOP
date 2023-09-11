a9a15a24adb1b576daef0705b7fa2849a3187696
zEkoR1 <komarovitstudies@gmail.com>
2023-09-11 12:47:08 +0300

    first commit

---
+++ .java
@@ -0,0 +1,21 @@
+public class Car {
+float speed = 0;
+boolean isMoving = false;
+public void stop() {
+isMoving = false;
+}
+public void accelerate(float amount) {
+speed += amount;
+isMoving = speed != 0;
+}
+public void printSpeed() {
+System.out.println("Speed: " + speed);
+}
+public void printIsMoving() {
+if(isMoving) {
+System.out.println("The car is moving");
+} else {
+System.out.println("The car is not moving");
+}
+}
+}
