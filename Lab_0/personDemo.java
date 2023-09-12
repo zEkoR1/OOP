e2a1151a4041b39614f1c8af3eaba461b2b929ad
zEkoR1 <komarovitstudies@gmail.com>
2023-09-12 18:23:16 +0300

    Fixed lab 0

---
+++ Lab_0/personDemo.java
@@ -0,0 +1,9 @@
+package Lab_0;
+public class personDemo {
+    public static void main(String[] args) {
+        Person person1 = new Person("John Doe", 30);
+        person1.printInfo();
+        person1.setAge(35);
+        person1.printInfo();
+    }
+}
