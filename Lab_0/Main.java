8ac04157ff44b8de8bcc1aab77045cf706712733
zEkoR1 <komarovitstudies@gmail.com>
2023-09-12 18:33:39 +0300

    Fixed lab 0

--- Lab_0/Main.java
+++ Lab_0/Main.java
@@ -1,5 +1,5 @@
 package Lab_0;
-public class personDemo {
+public class Main {
     public static void main(String[] args) {
         Person person1 = new Person("John Doe", 30);
         person1.printInfo();
