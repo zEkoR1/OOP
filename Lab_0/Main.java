9b6ce92d5643ee961ecb6767b0fc35bcc7596fc0
zEkoR1 <komarovitstudies@gmail.com>
2023-09-18 11:59:09 +0300

    Fixed lab 0

--- Lab_0/Main.java
+++
@@ -1,9 +0,0 @@
-package Lab_0;
-public class Main {
-    public static void main(String[] args) {
-        Person person1 = new Person("John Doe", 30);
-        person1.printInfo();
-        person1.setAge(35);
-        person1.printInfo();
-    }
-}
---
+++ Lab_0/Print_info_about_person.java
@@ -0,0 +1,7 @@
+package Lab_0;
+public class Print_info_about_person {
+    public static void main(String[] args) {
+        Person person1 = new Person("John john", 13);
+        person1.printInfo();
+    }
+}
