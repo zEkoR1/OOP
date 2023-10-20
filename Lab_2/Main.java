7d450d5d82703fd72b2bbf7326155f026ef8c3e7
PC <komarovitstudies@gmail.com>
2023-10-20 18:02:30 +0300

    Lab 3

---
+++ Lab_2/Main.java
@@ -0,0 +1,8 @@
+package Lab_2;
+import Lab_2.behaviour.ApplicationLoop;
+public class Main {
+    public static void main(String[] args) {
+        ApplicationLoop app = new ApplicationLoop();
+        app.run();
+    }
+}
